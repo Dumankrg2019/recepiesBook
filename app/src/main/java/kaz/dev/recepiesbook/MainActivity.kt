5de691909bfe214b98f587ac11e18c1cf0251aaa
Duman <52251067+Dumankrg2019@users.noreply.github.com>
2023-02-08 16:50:10 +0600

    feat: first build

---
+++ app/src/main/java/kaz/dev/recepiesbook/MainActivity.kt
@@ -0,0 +1,11 @@
+package kaz.dev.recepiesbook
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
