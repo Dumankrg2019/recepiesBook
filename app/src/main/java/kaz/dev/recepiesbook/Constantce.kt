ba35ac16dd8c9d4722f43c9bfa6122e7bbd44ec7
Duman <52251067+Dumankrg2019@users.noreply.github.com>
2023-02-28 17:53:43 +0600

    feat: add Network for hilt

--- app/src/main/java/kaz/dev/recepiesbook/Constantce.kt
+++ app/src/main/java/kaz/dev/recepiesbook/Constantce.kt
@@ -1,9 +1,8 @@
 package kaz.dev.recepiesbook
 
 class Constance {
-
     companion object {
-        const val BASE_IRL = "https://api.spoonacular.com/"
+        const val BASE_URL = "https://api.spoonacular.com/"
         const val API_KEY = "25c8c319ad764e97a8800a96610779a0"
     }
 }
---
+++ app/src/main/java/kaz/dev/recepiesbook/di/NetworkModule.kt
@@ -0,0 +1,54 @@
+package kaz.dev.recepiesbook.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.android.internal.managers.ApplicationComponentManager
+import kaz.dev.recepiesbook.Constance
+import kaz.dev.recepiesbook.FoodRecipesApi
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import java.util.concurrent.TimeUnit
+import javax.inject.Singleton
+
+@Module
+@InstallIn(ApplicationComponentManager::class)
+
+object NetworkModule {
+
+    @Singleton
+    @Provides
+    fun provideHttpClient(): OkHttpClient {
+        return OkHttpClient.Builder()
+            .readTimeout(15, TimeUnit.SECONDS)
+            .connectTimeout(15, TimeUnit.SECONDS)
+            .build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideConverterFactory():GsonConverterFactory {
+        return GsonConverterFactory.create()
+    }
+
+    @Singleton
+    @Provides
+    fun provideRetrofitInstance(
+        okHttpClient: OkHttpClient,
+        gsonConvertorFactory: GsonConverterFactory
+    ): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(Constance.BASE_URL)
+            .client(okHttpClient)
+            .addConverterFactory(gsonConvertorFactory)
+            .build()
+
+    }
+
+    @Singleton
+    @Provides
+    fun provideApiService(retrofit: Retrofit): FoodRecipesApi {
+        return retrofit.create(FoodRecipesApi::class.java)
+    }
+}
